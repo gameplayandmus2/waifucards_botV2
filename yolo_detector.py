c553d2cf2382f0a37376aa07ff56f6e669a89337
Glazunov Mikhail <glazunov.mikhail2013@gmail.com>
2025-12-02 23:59:39 +0300

    Design UPD Approoved by Hezeka

--- yolo_detector.py
+++ yolo_detector.py
@@ -27,30 +27,43 @@ def draw_boxes_with_numbers(np_img, boxes):
 
     # Яркие читаемые цвета (BGR для OpenCV)
     COLORS = [
-        (0, 255, 0),     # зелёный
-        (0, 128, 255),   # оранжево-голубой
-        (255, 0, 0),     # синий
-        (255, 0, 255),   # фиолетовый p.s. от twinZ'a нихуя не совпадвает, но хоть номер и рамка одного цвета
-        (0, 255, 255),   # жёлтый
-        (255, 255, 0),   # голубой
-        (128, 0, 255),   # фиолетово-синий
-        (0, 0, 255),     # красный
+        (56, 150, 255),   # #3896ff
+        (50, 219, 137),   # #32db89
+        (255, 204, 0),    # #ffcc00
+        (226, 70, 70),    # #e24646
+        (255, 104, 38),   # #ff6826
+        (67, 74, 247),    # #434af7
+        (240, 99, 179),   # #f063b3
+        (58, 232, 218),   # #3ae8da
+        (160, 238, 25),   # #a0ee19
+        (163, 177, 197),  # #a3b1c5
+        (36, 36, 36),     # #242424
+
     ]
 
-    for i, (x1, y1, x2, y2) in enumerate(boxes, start=1):
-        color = COLORS[(i - 1) % len(COLORS)]
-
-        # Рисуем рамку OpenCV (BGR)
-        cv2.rectangle(img_np, (x1, y1), (x2, y2), color[::-1], 3)
-
-    # Конвертируем в PIL для текста (RGB)
     img_pil = Image.fromarray(img_np[..., ::-1])
     draw = ImageDraw.Draw(img_pil)
-    font = ImageFont.truetype("arial.ttf", max(24, (x2-x1)//5))  # размер адаптивный
+    
 
     for i, (x1, y1, x2, y2) in enumerate(boxes, start=1):
         color = COLORS[(i - 1) % len(COLORS)]
-        text = str(i)
+        # Конвертируем в PIL для текста (RGB)
+        
+
+        # Рисуем рамку OpenCV (BGR)
+        draw.rounded_rectangle(
+            [x1, y1, x2, y2],
+            radius=7,
+            outline=color,
+            width=8
+        )
+
+    font = ImageFont.truetype("Montserrat-Bold.ttf", max(12, (x2-x1)//10))  # размер адаптивный
+
+
+    for i, (x1, y1, x2, y2) in enumerate(boxes, start=1):
+        color = COLORS[(i - 1) % len(COLORS)]
+        text = str(i)        
 
         # Размер текста
         text_w, text_h = draw.textbbox((0,0), text, font=font)[2:]
@@ -59,7 +72,7 @@ def draw_boxes_with_numbers(np_img, boxes):
         box_cy = (y1 + y2) / 2
 
         # Положение текста с белым скруглённым прямоугольником
-        padding = 6
+        padding = 12
         rect_x1 = box_cx - text_w/2 - padding
         rect_y1 = box_cy - text_h/2 - padding
         rect_x2 = box_cx + text_w/2 + padding
@@ -68,13 +81,13 @@ def draw_boxes_with_numbers(np_img, boxes):
         # Скругленный прямоугольник
         draw.rounded_rectangle(
             [rect_x1, rect_y1, rect_x2, rect_y2],
-            radius=8,
+            radius=15,
             fill=(255,255,255)
         )
 
         # Текст по центру
         draw.text(
-            (box_cx - text_w/2, box_cy - text_h/2),
+            (box_cx - text_w/2, box_cy - text_h/2 - 7),
             text,
             font=font,
             fill=color
